b3852831e2569466d9a120018a90bb97b47826d4
Jakeer040694 <101064440+Jakeer040694@users.noreply.github.com>
2024-07-04 10:36:57 +0530

    Update install-package.sh

--- install-package.sh
+++ install-package.sh
@@ -5,4 +5,13 @@ then
 echo " you don't have root access please run with root access "
 else 
 echo " you are installing with root access "
+echo " please enter your package name "
+fi
+read package
+yum install $package -y
+if [ $? -eq 0 ];
+then
+echo " you are successfully installed $package "
+else
+echo " you are not installed package "
 fi
